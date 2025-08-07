8c61fce35ec5754f1dcae22f48a9f864afd6125c
Evan-1-code <evansantos286@gmail.com>
2025-08-07 16:56:49 +0800

    Revert "basic scene switches"
    This reverts commit aa8703e125129a5a8db51eece06e65c4fe8fc3cc.

--- algeventure_102/Scene/entrance_town_map_2.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var destination_scene: PackedScene
-@export var destination_spawn_name: String = "spawn_point"
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		body.set_meta("spawn_tag", "town_entrance_2")  # must match node name in next scene
-		get_tree().change_scene_to_file("res://Scene/town_map_2.tscn")
