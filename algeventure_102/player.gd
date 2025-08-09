6805198b92d0c9f514f706f531f440dce9ebaffd
Evan-1-code <evansantos286@gmail.com>
2025-08-09 20:36:09 +0800

    test movement (failed)
    issue: limited and slow movement, snaps back to original place after letting go of keys

---
+++ algeventure_102/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const ACCELERATION = 15.0
+const FRICTION = 10.0
+
+func _physics_process(delta):
+	var input_vector = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	).normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	move_and_slide()
