631b24a5eb719ae5dc46ff57af27e55360892734
redpokuya <renosaredangel@gmail.com>
2025-08-16 09:37:43 +0800

    player movement animation

--- algeventure_102/player.gd
+++ algeventure_102/player.gd
@@ -1,17 +1,30 @@
 extends CharacterBody2D
 
+@export var move_speed: float = 100.0
+var last_direction = "down"
 
-@export var move_speed : float = 100
+func _physics_process(delta):
+	var direction = Vector2.ZERO
 
-func _physics_process(_delta):
-	
-	var input_direction =Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
-	
-	
-	velocity=input_direction * move_speed
-	
-	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+
+	direction = direction.normalized()
+	velocity = direction * move_speed
 	move_and_slide()
+
+	if direction != Vector2.ZERO:
+		if abs(direction.x) > abs(direction.y):
+			last_direction = "right" if direction.x > 0 else "left"
+		else:
+			last_direction = "down" if direction.y > 0 else "up"
+
+		$AnimatedSprite2D.play("walk_" + last_direction)
+	else:
+		$AnimatedSprite2D.play("idle_" + last_direction)
